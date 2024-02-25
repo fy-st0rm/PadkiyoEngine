08722f8ac35ccd1ebd238c925fda031efcda7d3b
fy-st0rm <slokpradhan2062@gmail.com>
2024-02-25 13:55:29 +0545

    Updated some namings.

--- src/math/utils.h
+++ src/math/utils.h
@@ -4,9 +4,10 @@
 #include "core/includes.h"
 #include "core/defines.h"
 
-#define ISO_PI 3.14159
-#define iso_radians(x) x * ISO_PI / 180
-#define iso_rand_range(l, u) rand() % (u - l + 1) + l
+#define PE_PI 3.14159
+#define pe_radians(x) x * PE_PI / 180
+#define pe_rand_init(seed) srand(seed)
+#define pe_rand_range(l, u) rand() % (u - l + 1) + l
 
 static b8 f32_eq(f32 a, f32 b) {
 	return fabs(a - b) < 0.01f;
